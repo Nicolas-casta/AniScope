7c74108bbd5b7dd980fd70c8f5378e2807a7d989
Nicolas-casta <nicolascastano2008@gmail.com>
2026-04-18 23:55:29 -0500

    feat: agregar barrel export del design system

---
+++ src/design/index.ts
@@ -0,0 +1,14 @@
+export { default as Badge } from './atoms/badge'
+export { default as Button } from './atoms/button'
+export { default as Input } from './atoms/input'
+export { default as Skeleton } from './atoms/skeleton'
+
+export { default as SearchBar } from './molecules/search-bar'
+export { default as FilterChip } from './molecules/filter-chip'
+export { default as ErrorMessage } from './molecules/error-message'
+
+export { default as AnimeCard } from './organisms/anime-card'
+export { default as AnimeCardSkeleton } from './organisms/anime-card-skeleton'
+
+export { default as ListLayout } from './templates/list-layout'
+export { default as DetailLayout } from './templates/detail-layout'
