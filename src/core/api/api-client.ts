72771b8e99a682baafec7e4d3102ef37f1e19472
Nicolas-casta <nicolascastano2008@gmail.com>
2026-04-18 22:43:51 -0500

    feat: configurar api client y constantes

---
+++ src/core/api/api-client.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import { BASE_URL } from '../config/api.config'
+
+const apiClient = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+})
+
+export default apiClient
---
+++ src/core/config/api.config.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.jikan.moe/v4'
+export const PAGE_LIMIT = 20
